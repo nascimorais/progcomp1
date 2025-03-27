b0b82abf405362f19e43591a9f95084e0cb51b61
FELIPE NASCIMENTO DE MORAIS <26150@unifacef.edu.br>
2025-03-26 22:18:07 -0300

    alo

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,10 +1,20 @@
 function calcular(){
 //recupera o valor da ação social digitado
 // decaração variável sem tipo
+let soma = 0
 let acaoSocial = document.getElementById("acaoSocial").value
+soma = soma + Number(acaoSocial)
 
 let homenagem = document.getElementById("homenagem").value
+soma = soma + Number (homenagem)
 //vamos somar valores
-let soma = Number(acaoSocial) + Number(homenagem)
-alert(soma)
+let leite = document.getElementById("leite").value
+soma = soma + (2 * Number(leite))
+
+let KitAvulso = document.getElementById("kitAvulso").value
+soma = soma + Number( 30 * KitAvulso)
+let suplementoAvulso = document.getElementById("suplementoAvulso").value
+soma = soma + Number( 15 * Number(suplementoAvulso))
+
+document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
